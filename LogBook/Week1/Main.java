994dbb31adb67a1a9724495e1b1ff99237e523ad
JamesSmith <100181420@student.burnley.ac.uk>
2025-02-02 19:03:45 +0000

    added code for task 5

--- LogBook/Week1/Main.java
+++ LogBook/Week1/Main.java
@@ -4,40 +4,48 @@ import java.util.Scanner;
 
 public class Main
 {
-    static void hellow (){
+    static void hellow ()
+    {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name: ");
         String name = scanner.next();
-        System.out.println ("hello " + name);
-
+        System.out.println("hello " + name);
     }
     static void rectangale()
     {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter lenth number one: ");
         int len1  = scanner.nextInt();
-        System.out.println ("Enter lenth number one: ");
+        System.out.println ("Enter lenth number two: ");
         int len2  = scanner.nextInt();
         int aria = len1 * len2;
-        System.out.println("the aria is" + aria);
+        System.out.println("the aria is: " + aria);
     }
-        static void avrage()
-        {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("enter number 1");
-            double num1 = scanner.nextDouble();
-            System.out.println("enter number 2");
-            double num2 = scanner.nextDouble();
-            double ans = (num1 + num2)/2;
-            System.out.println("the avrage is "+ ans);
+    static void avrage()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter number 1");
+        double num1 = scanner.nextDouble();
+        System.out.println("enter number 2");
+        double num2 = scanner.nextDouble();
+        double ans = (num1 + num2)/2;
+        System.out.println("the avrage is "+ ans);
 
-        }
+    }
 
-    
-
+    static void task5()
+    {
+     Scanner scanner = new Scanner(System.in);
+     System.out.println("enter number 1: ");
+     double num1 = scanner.nextDouble();
+     System.out.println("enter number 2: ");
+     double num2 = scanner.nextDouble();
+     System.out.println(num1 +" "+ num2);
+     System.out.println(num2 +" " + num1);
+    }
     public static void main (String []Args)
     {
-        Main.hellow() ;
+        task5();
 
     }
 }
