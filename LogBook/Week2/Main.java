31aecaf4ad01d0babc181c8aab2b13dffa41c754
JamesSmith <100181420@student.burnley.ac.uk>
2025-02-03 19:21:30 +0000

    added task 2 week 2

--- LogBook/Week2/Main.java
+++ LogBook/Week2/Main.java
@@ -16,9 +16,40 @@ public class Main
             System.out.println("You are 18 or older");
     }
 
+    static void task2() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your grade A,B,C,D,E,F");
+        String original_grade = scanner.next();
+        original_grade = original_grade.toLowerCase();
+        String new_grade = "";
+        switch (original_grade) {
+            case "a":
+                new_grade = "1st";
+                break;
+            case "b":
+                new_grade = "2:1";
+                break;
+            case "c":
+                new_grade = "2:2";
+                break;
+            case "d":
+                new_grade = "3rd";
+                break;
+            case "e":
+                new_grade = "ordanary";
+                break;
+            case "f":
+                new_grade = "Fail";
+                break;
+            default:
+                new_grade = "invalid value enterd";
+                break;
+        }
+        System.out.println("your university grade is " + new_grade);
+    }
     public static void main(String args[])
     {
-        task1();
+        task2();
 
     }
 }
